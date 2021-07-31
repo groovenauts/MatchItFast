311b2e275eab3a3cd3805af3ecfeeaae8ea1c29f
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2021-07-31 21:53:33 +0900

    Implement intro page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-title">
+          Match It Fast
+        </div>
+        <div className="App-subtitle">
+          Porwered by Vertex AI Matching Engine.
+        </div>
+        <div className="App-start">
+          Start
+        </div>
       </header>
     </div>
   );
