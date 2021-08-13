51ed63142b10d99d9e8f80e2791f88d623777ea5
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2021-08-13 16:45:38 +0900

    gsg-encoder: Export http port to all interfaces.

--- gsg-encoder/main.py
+++ gsg-encoder/main.py
@@ -33,4 +33,4 @@ def sentence_embedding():
     return jsonify({ "embedding": UniversalSentenceEncoder.instance().embed(j["text"])})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(host='0.0.0.0', port=80)
