0a939fa7641bb0ea213b975496eccf3df707d192
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2021-08-15 20:21:43 +0900

    Use async/await in the mobileNetV2 module.

--- src/mobileNetV2.ts
+++ src/mobileNetV2.ts
@@ -2,23 +2,19 @@ import * as tf from '@tensorflow/tfjs';
 
 var model: any = null;
 
-export const loadModel = () => {
+export const loadModel = async () => {
   console.log("start loading mobileNetV2");
-  return new Promise((resolve, reject) => {
-    tf.loadGraphModel("https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_100_224/feature_vector/3/default/1", { fromTFHub: true }).then((net) => {
-      console.log("loaded mobileNetV2");
-      // warm up;
-      tf.tidy(() => {
-          net.execute(tf.zeros([1, 224, 224, 3]));
-          console.log("warmed up mobileNetV2");
-      });
-      model = net;
-      resolve(model);
-    });
+  const net = await tf.loadGraphModel("https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_100_224/feature_vector/3/default/1", { fromTFHub: true });
+  console.log("loaded mobileNetV2");
+  tf.tidy(() => {
+    net.execute(tf.zeros([1, 224, 224, 3]));
   });
+  console.log("warmed up mobileNetV2");
+  model = net;
+  return net;
 };
 
-export const inference = (image_tag: HTMLImageElement) => {
+export const inference = async (image_tag: HTMLImageElement) => {
   if (model === null) {
     return null;
   }
@@ -30,12 +26,7 @@ export const inference = (image_tag: HTMLImageElement) => {
   tf.dispose(raw_image);
   const result = tf.tidy(() => model.execute(image));
   tf.dispose(image);
-  return new Promise((resolv: any, reject: any) => {
-    if (result) {
-      result.data().then((embedding: Float32Array) => {
-        resolv(Array.from(embedding))
-        tf.dispose(result);
-      });
-    }
-  });
+  const embedding = await result.data();
+  tf.dispose(result);
+  return Array.from(embedding);
 };
