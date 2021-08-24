3d7001743caf0ace4eafea78a724065ae1f8c742
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2021-08-24 16:24:11 +0900

    Add a link to Google News on the News Search page.

--- src/NewsForm.tsx
+++ src/NewsForm.tsx
@@ -17,6 +17,7 @@ function NewsForm(props: Props) {
     <div className="NewsForm">
       <div className="NewsForm-title">Enter query text</div>
       <div className="NewsForm-subtitle">(currently only available in English)</div>
+      <div className="NewsForm-subtitle">You can find the latest news articles from <a href="https://news.google.com/?hl=en-US" target="_blank" rel="noreferrer" style={{"color": "inherit"}} >Google News</a>.</div>
       <textarea className="NewsForm-textarea" ref={textareaRef} />
       <div className="NewsForm-query-button" onClick={() => dispatch(actions.queryArticle(textareaRef.current!.value))} >Query</div>
       <div key="back" className="reset-button" onClick={() => dispatch(actions.start())} >Back </div>
